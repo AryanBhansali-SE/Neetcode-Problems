08ed05617ea6608987874abcbbada999749f8792
AryanBhansali-SE <aryan.bhansali2004@gmail.com>
2026-05-09 15:38:14 +0530

    Create Top_K_Frequent_Elements.py

---
+++ Arrays and Hashing/Top_K_Frequent_Elements.py
@@ -0,0 +1,18 @@
+#Given an integer array nums and an integer k, return the k most frequent elements within the array.
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
